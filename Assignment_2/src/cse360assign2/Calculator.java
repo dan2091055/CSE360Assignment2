f1a62444644c0b6ffd192ff8526bd716c7d44c2e
daniel <daniel@172.20.10.3>
2019-02-25 20:40:48 -0700

    This is going to be the first version of the calculator

---
+++ Assignment_2/src/cse360assign2/Calculator.java
@@ -0,0 +1,78 @@
+package cse360assign2;
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * @author Daniel Martinez
+ * Class ID: 475
+ * Project description: below are some of the commands that we need to code
+ * 
+ * @author Daniel Martinez
+ *
+ */
+
+public class Calculator {
+	
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * this is going to be returning the total value that has been calculated.
+	 * @return
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * this is going to be adding the total and the value together.
+	 * 
+	 * 
+	 * @param value
+	 */
+	
+	public void add (int value) {
+		
+	}
+	/**
+	 * The function below is going to be subtracting the value from the total.
+	 * 
+	 * @param value
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * The multiply function is going to multiply the total with the value parameter
+	 * 
+	 * 
+	 * @param value
+	 */
+	
+	public void multiply (int value) {
+		
+	}
+	/**
+	 * The divide function is going divide the total by the paramater value,
+	 * but if the value is equal to zero then we set the total equal to zero
+	 * 
+	 * @param value
+	 */
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+
+}
